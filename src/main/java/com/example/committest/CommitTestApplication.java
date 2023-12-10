e42762f94e5c410b85237730f5ce744a00b262f6
mynuni <elelaka0000@gmail.com>
2023-12-10 14:21:58 +0900

    first commit

---
+++ src/main/java/com/example/committest/CommitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.committest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/committest/CommitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.committest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
